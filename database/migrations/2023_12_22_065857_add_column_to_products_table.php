042609062b4225f695ad7bd42e6d870815c45ae4
JaganML1993 <jagan.ml1993@gmail.com>
2023-12-24 19:19:10 +0530

    migration changes

--- database/migrations/2023_12_22_065857_add_column_to_products_table.php
+++ database/migrations/2023_12_22_065857_add_column_to_products_table.php
@@ -18,9 +18,9 @@ return new class extends Migration
             $table->string('image1')->nullable();
             $table->string('image2')->nullable();
             $table->string('role_of_deficiency')->nullable();
-            $table->string('role_description')->nullable();
+            $table->text('role_description')->nullable();
             $table->string('deficiency')->nullable();
-            $table->string('deficiency_description')->nullable();
+            $table->text('deficiency_description')->nullable();
         });
     }
 
